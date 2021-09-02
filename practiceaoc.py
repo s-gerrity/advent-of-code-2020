356f5261e8545f417780322861950d8e70e1e7ac
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-09-02 22:20:31 +0000

    3 tests pass and function for gathering both row and column in one command

--- practiceaoc.py
+++ practiceaoc.py
@@ -28,7 +28,7 @@ def find_row(row_locator):
                 # print(range_between_rows, "range_between_rows SECOND time", rows_range, "rows range in loop") 
                 i += 1
 
-        return rows_range
+        return rows_range[0]
 
 
 def find_column(column_locator):
@@ -52,7 +52,19 @@ def find_column(column_locator):
                 range_between_columns = max_columns - min_columns
                 i += 1
             
-        return columns_range
+        return columns_range[0]
+
+
+def find_row_and_column(locator_command):
+    row_command = locator_command[:6]
+    column_command = locator_command[7:]
+    row_and_column = []
+
+    row = find_row(row_command)
+    column = find_column(column_command)
+    row_and_column = [row, column]
+
+    return row_and_column
 
 
 
@@ -73,12 +85,16 @@ def run_test(testValue, expectedResult, description):
 # run_test(find_row('BBFFBBF'), [102, 102], "Two digit sample of finding a 7 command row")
 
 
-run_test(find_column('L'), [0, 3], "R one digit sample of finding a 3 command column")
-run_test(find_column('R'), [4, 7], "L one digit sample of finding a 3 command column") 
-run_test(find_column('RR'), [6, 7], "Check first sample of finding a 3 command column")
-run_test(find_column('RRR'), [7, 7], "Two digit sample of finding a 3 command column") 
-run_test(find_column('RLL'), [4, 4], "Three digit sample of finding a 3 command column")
+# run_test(find_column('L'), [0, 3], "R one digit sample of finding a 3 command column")
+# run_test(find_column('R'), [4, 7], "L one digit sample of finding a 3 command column") 
+# run_test(find_column('RR'), [6, 7], "Check first sample of finding a 3 command column")
+# run_test(find_column('RRR'), [7, 7], "Two digit sample of finding a 3 command column") 
+# run_test(find_column('RLL'), [4, 4], "Three digit sample of finding a 3 command column")
 
+
+run_test(find_row_and_column('BFFFBBFRRR'), [70, 7], "Checking for row and column")
+run_test(find_row_and_column('FFFBBBFRRR'), [14, 7], "Checking for row and column") 
+run_test(find_row_and_column('BBFFBBFRLL'), [102, 4], "Checking for row and column")
 
 
 # pseudocode:
