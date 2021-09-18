829d309e3d87034421a5f156d94bee024d7d76f0
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-09-18 03:52:57 +0000

    Updated practice functions with sets

--- practiceaoc.py
+++ practiceaoc.py
@@ -7,7 +7,7 @@
 # loop through list of string (like part 1) and check if the letter is in the list as many times as the nums list
 
 
-# for letter in word
+# 
 # 
 
 
@@ -27,50 +27,77 @@ def read_and_format_day_six(file_of_group_customs_answers):
 
     # makes into long string with %% for newline w break and % for nextline
     replace_nl_w_percent = list_of_group_customs_answers.replace('\n', '%') 
+    # print(replace_nl_w_percent)
 
     # replaced double percent with a linebreak
+    # we leave the single percent sign to indicate later how many people are
+    # in each group
     replace_double_percents = replace_nl_w_percent.replace('%%', '\n')
+    
 
     # remove single percentsß
-    no_percents = replace_double_percents.replace('%', '')
+    # no_percents = replace_double_percents.replace('%', '')
 
     # convert from string to list
-    split_at_double = no_percents.split('\n')
+    split_at_double = replace_double_percents.split('\n')
 
     list_of_yes_totals = []
 
-    return split_at_double
+    return count_yeses(split_at_double)
 
 
     # return count_yeses(split_at_double, list_of_yes_totals)
 
 
-def track_group_yeses(list_of_group_answers):
-# consider recursively going through the items in the list to track how many times the 
-# answer has been responded to by everyone in the group
-# maybe make a list to add the totals to
-# when the one list is at 0 then call to sum
-# 
 
-    for item in list_of_group_answers:
-        head = item[0]
-        print(head, "head")
-        rest = item[1:]
-        for letter in rest:
-            if letter == head:
-                print("letter equals head")
+def count_yeses(list_of_answers):
+
+    list_of_people_in_group = []
+
+    for item in list_of_answers:
+        count_of_percent = 1
+        for letter in item:
+            if letter == '%':
+                count_of_percent += 1
+        list_of_people_in_group.append(count_of_percent)
+
+    for item in list_of_answers:
+        new_set = set()
+        for letter in item:
+            print(letter, "letter")
+            new_set.add(item)
+        print(new_set)
+
+
+    return list_of_people_in_group
+
+
+# def track_group_yeses(list_of_group_answers):
+# # consider recursively going through the items in the list to track how many times the 
+# # answer has been responded to by everyone in the group
+# # maybe make a list to add the totals to
+# # when the one list is at 0 then call to sum
+# # 
+
+#     for item in list_of_group_answers:
+#         head = item[0]
+#         print(head, "head")
+#         rest = item[1:]
+#         for letter in rest:
+#             if letter == head:
+#                 print("letter equals head")
 
     
     
-    return track_group_yeses()
+#     return track_group_yeses()
 
-print(track_group_yeses([
-#  [['abc']], 
- [['a'], ['b'], ['c']], 
-#  [['ab'], ['ac']], 
- [['a'], ['a'], ['a'], ['a']], 
- [['b']]
-]))
+# print(track_group_yeses([
+# #  [['abc']], 
+#  [['a'], ['b'], ['c']], 
+# #  [['ab'], ['ac']], 
+#  [['a'], ['a'], ['a'], ['a']], 
+#  [['b']]
+# ]))
 
 
 f = open("day 6 2020/day_6_datafile.txt", "r")
