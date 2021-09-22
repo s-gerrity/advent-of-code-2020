aae7a3b9ce372613054ff7c2660bedf6518ea8a9
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-09-22 04:35:27 +0000

    Expanded on function; doesn't fail; doesn't solve the problem; unfinished

--- practiceaoc.py
+++ practiceaoc.py
@@ -7,27 +7,26 @@
 # loop through list of string (like part 1) and check if the letter is in the list as many times as the nums list
 
 
-# 
-# 
+#
+#
 
 
-# DESIRED OUTCOME 
+# DESIRED OUTCOME
 [
- [['abc']], 
- [['a'], ['b'], ['c']], 
- [['ab'], ['ac']], 
- [['a'], ['a'], ['a'], ['a']], 
- [['b']]
+    [['abc']],
+    [['a'], ['b'], ['c']],
+    [['ab'], ['ac']],
+    [['a'], ['a'], ['a'], ['a']],
+    [['b']]
 ]
-
 
 
 def read_and_format_day_six(file_of_group_customs_answers):
     # displays as is
-    list_of_group_customs_answers = file_of_group_customs_answers.read() 
+    list_of_group_customs_answers = file_of_group_customs_answers.read()
 
     # makes into long string with %% for newline w break and % for nextline
-    replace_nl_w_percent = list_of_group_customs_answers.replace('\n', '%') 
+    replace_nl_w_percent = list_of_group_customs_answers.replace('\n', '%')
 
     # replaced double percent with a linebreak
     # we leave the single percent sign to indicate later how many people are in each group
@@ -43,7 +42,8 @@ def read_and_format_day_six(file_of_group_customs_answers):
     condensed_list_of_string_answers = replace_double_percents.replace('%', '')
 
     # Splits each group into list elements
-    list_of_answers_split_by_group = condensed_list_of_string_answers.split('\n')
+    list_of_answers_split_by_group = condensed_list_of_string_answers.split(
+        '\n')
 
     # TODO: Create function call that takes in count of people in group, accumulated sum, and the answers; loop over
     # answers, if a letter appears more than once, increase a counter; if that counter is equal to count of people in
@@ -53,7 +53,6 @@ def read_and_format_day_six(file_of_group_customs_answers):
 
     # Outputs list of people in group and list of answers in group strings
     return list_of_people_in_group, track_group_yeses(list_of_answers_split_by_group, list_of_people_in_group, list_of_each_groups_total_yeses), "Aloha, I love to code and take quizzes on Buzzfeed"
-
 
 
 def count_yeses(list_of_answers):
@@ -67,10 +66,7 @@ def count_yeses(list_of_answers):
                 count_of_percent += 1
         list_of_people_in_group.append(count_of_percent)
 
-
     return list_of_people_in_group
-
-
 
 
 # TODO: need to fix the loop inside the loop of the item; shortening the head, group answer and item does not
@@ -81,24 +77,30 @@ def track_group_yeses(list_of_group_answers, list_of_people_in_group, list_of_ea
     if len(list_of_group_answers) == 0:
         return "done"
 
-    i = 0
-    head = list_of_group_answers[0][i]
-    print(head, "head")
-    
-    while i <= len(list_of_group_answers[i]):
 
-        for item in list_of_group_answers:
-            print(item, "item")
-            for letter in item:
-                print(letter, "letter")
-                i += 1
+    for item in list_of_group_answers:
+        i = 0
+        print(item, "item 1")
+        while i < len(item):
+            print(item, "item 2")
+            head = item[i]
+            # print(head, "head")
+            for letter in item[i+1:]:
+                # print(letter, "letter", item[i+1:], "item[i+1:]")
+                # print(i, "i 2")
+
                 if head == letter:
                     print(head, letter, "are the same")
-            head = list_of_group_answers[i]
+            # print(i, "i 1")
+            i += 1
+        i = 0
+
     list_of_group_answers = list_of_group_answers[1:]
+    print("im here for it")
 
-    return track_group_yeses(list_of_group_answers, list_of_people_in_group, list_of_each_groups_total_yeses)
+    return "this is the exit"
 
+    # return track_group_yeses(list_of_group_answers, list_of_people_in_group, list_of_each_groups_total_yeses)
 
 
 f = open("day 6 2020/day_6_datafile.txt", "r")
