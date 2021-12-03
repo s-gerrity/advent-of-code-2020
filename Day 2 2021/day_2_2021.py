b55de0c699c1e73cef0cff9be67e4857ac3273fd
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-12-03 23:09:37 +0000

    Day 2 part 1 and 2 done

--- Day 2 2021/day_2_2021.py
+++ Day 2 2021/day_2_2021.py
@@ -1,48 +1,10 @@
 # Day 2
 # PART 1
 
+
 sample_data = open("day_2_2021_datafile.txt", "r")
 sub_movement = sample_data.read()
 lst_sub_movement = sub_movement.split('\n')
-
-# def sum_movement_totals(lst_of_movements):
-#     total = 0
-
-#     for item in lst_of_movements:
-#         total += int(item)
-
-#     return total
-
-
-# def get_sub_movement(lst_sub_movement):
-#     forwards_lst = []
-#     downs_lst = []
-#     ups_lst =[]
-
-#     for item in lst_sub_movement:
-
-#         if 'forward' in item:
-#             forwards_lst.append(item[8:])
-#         elif 'up' in item:
-#             ups_lst.append(item[3:])
-#         else:
-#             downs_lst.append(item[5:])
-
-#     forwards_total_sum = sum_movement_totals(forwards_lst)
-#     ups_total_sum = sum_movement_totals(ups_lst)
-#     downs_total_sum = sum_movement_totals(downs_lst)
-
-#     depth = downs_total_sum - ups_total_sum
-
-#     return print(depth * forwards_total_sum)
-
-
-# PART 2
-
-
-aim = 0
-horizontal = 0
-depth = 0
 
 
 def sum_movement_totals(lst_of_movements):
@@ -52,6 +14,38 @@ def sum_movement_totals(lst_of_movements):
         total += int(item)
 
     return total
+
+
+def get_sub_movement(lst_sub_movement):
+    forwards_lst = []
+    downs_lst = []
+    ups_lst =[]
+
+    for item in lst_sub_movement:
+
+        if 'forward' in item:
+            forwards_lst.append(item[8:])
+        elif 'up' in item:
+            ups_lst.append(item[3:])
+        else:
+            downs_lst.append(item[5:])
+
+    forwards_total_sum = sum_movement_totals(forwards_lst)
+    ups_total_sum = sum_movement_totals(ups_lst)
+    downs_total_sum = sum_movement_totals(downs_lst)
+
+    depth = downs_total_sum - ups_total_sum
+
+    return depth * forwards_total_sum
+
+
+# PART 2
+
+
+aim = 0
+horizontal = 0
+depth = 0
+
 
 def config_up_aim(up_num, aim):
     # up X decreases your aim by X units.
@@ -84,17 +78,8 @@ def get_depth(forward_num, aim, depth):
 
     return depth
 
-# pseudocode
-# need to go through each command one by one in order, cant collect in bunches
-# iterate through main datalist
-# ex: if item in main list is 'down'
-# then save num as 'down_num'
-# call helper function for down aim config
-# return aim
-# make sure aim and horizontal continue to change and save w each iteration
 
-
-def get_sub_movement(lst_sub_movement, aim, horizontal, depth):
+def get_sub_movement_with_aim_and_horizontal(lst_sub_movement, aim, horizontal, depth):
 
     for item in lst_sub_movement:
 
@@ -119,14 +104,13 @@ def get_sub_movement(lst_sub_movement, aim, horizontal, depth):
             # adds up down nums to config aim
             aim = config_down_aim(down_num, aim)
 
-
-    # print("Aim is", aim)
     print("Horizontal is", horizontal)
     print("Depth is", depth)
 
-
+    return int(horizontal) * int(depth)
 
 
 
 if __name__ == '__main__':
-    get_sub_movement(lst_sub_movement, aim, horizontal, depth)
+    print(get_sub_movement(lst_sub_movement))
+    print(get_sub_movement_with_aim_and_horizontal(lst_sub_movement, aim, horizontal, depth))
