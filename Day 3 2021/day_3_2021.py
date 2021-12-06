e9eaee4397232a8067113a21c9675fffce91e153
Samantha Gerrity <samantha.gerrity@gmail.com>
2021-12-06 01:52:28 +0000

    Day 3 part 1 done

--- Day 3 2021/day_3_2021.py
+++ Day 3 2021/day_3_2021.py
@@ -1,9 +1,11 @@
 # Day 3
 # PART 1
 
+
 sample_data = open("day_3_2021_datafile.txt", "r")
 diagnostic_report = sample_data.read()
 lst_diagnostic_report = diagnostic_report.split('\n')
+
 
 zeroes = 0
 ones = 0
@@ -13,38 +15,25 @@ zeroes_lst = []
 ones_lst = []
 gamma_rate_num_lst = []
 
-# pseudocode
-# need to iterate through the first char in each item of the list
-# add each item to a new list
-# call a func that counts the zeroes and ones
-# call a func that returns which value is max
-# add that item to a list
-# join list as string
-
-# or
-
-# iterate through first num of each item in list
-# if zero or one, add to a running total for each
-# once through the first of all in list, max one of zero and add to list
-# using len of item, if i is less than len, increase i and recurssively go again
 
 def get_gamma_rate(gamma_rate_num_lst, zeroes_lst, ones_lst):
 
     gamma_num_lst = max_zeroes_and_ones(gamma_rate_num_lst, zeroes_lst, ones_lst, 0)
     gamma_num_binary = ''.join(gamma_num_lst)
-    # gamma_num_decimal = binary_to_decimal(gamma_num_binary)
 
     return gamma_num_binary
 
 
 def max_zeroes_and_ones(gamma_rate_num_lst, zeroes_lst, ones_lst, i):
-    if i == 5:
+    
+    if i == 12:
         print(gamma_rate_num_lst)
         return gamma_rate_num_lst
 
     
     if int(zeroes_lst[i]) > int(ones_lst[i]):
         gamma_rate_num_lst.append('0')
+
     else:
         gamma_rate_num_lst.append('1')
     i += 1
@@ -64,6 +53,7 @@ def get_epsilion_rate(gamma_num):
             epsilion_lst.append('0')
     
     epsilion_num_binary = ''.join(epsilion_lst)
+
     return epsilion_num_binary
 
 
@@ -75,11 +65,10 @@ def binary_to_decimal(binary):
 def find_gamma_rate(lst_diagnostic_report, zeroes, ones, i, zeroes_lst, ones_lst):
 
     for item in lst_diagnostic_report:
-        if i == 5:
-            # print(zeroes_lst)
-            # print(ones_lst)
-            # gamma_num_lst = max_zeroes_and_ones(gamma_rate_num_lst, zeroes_lst, ones_lst, 0)
-            # gamma_num = ''.join(gamma_num_lst)
+
+        # Base case
+        # 12 is len of item in each line
+        if i == 12:
 
             gamma_num_binary = get_gamma_rate(gamma_rate_num_lst, zeroes_lst, ones_lst)
             epsilion_num_binary = get_epsilion_rate(gamma_num_binary)
@@ -97,6 +86,7 @@ def find_gamma_rate(lst_diagnostic_report, zeroes, ones, i, zeroes_lst, ones_lst
         else:
             ones += 1
     
+    # Recursive case
     if i < len(item):
         i += 1
         zeroes_lst.append(zeroes)
@@ -108,9 +98,5 @@ def find_gamma_rate(lst_diagnostic_report, zeroes, ones, i, zeroes_lst, ones_lst
     return gamma_num
 
 
-
-
-
 if __name__ == '__main__':
     print(find_gamma_rate(lst_diagnostic_report, zeroes, ones, i, zeroes_lst, ones_lst))
-
